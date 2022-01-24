1dc8d69b62eafc6328ce3d7d6bea3ad68099f7c2
akmalsyrf <dev.akmal69@gmail.com>
2022-01-25 01:01:13 +0700

    add movement to the obstacles

--- physics.js
+++ physics.js
@@ -1,5 +1,11 @@
 import Matter from "matter-js";
 
+import { Dimensions } from "react-native";
+import { getPipeSizePosPair } from "./utils/random";
+
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
+
 const Physics = (entities, { touches, time, dispatch }) => {
   let engine = entities.physics.engine;
 
@@ -10,6 +16,20 @@ const Physics = (entities, { touches, time, dispatch }) => {
     });
 
   Matter.Engine.update(engine, time.delta);
+
+  //movement the obstacle
+  for (let i = 1; i <= 2; i++) {
+    //looping forever because the condition is never fulfilled
+    if (entities[`ObstacleTop${i}`].body.bounds.max.x <= 0) {
+      // console.log(entities[`ObstacleTop${i}`].body.bounds.max.x);
+      const pipeSizePos = getPipeSizePosPair(windowWidth * 0.9);
+
+      Matter.Body.setPosition(entities[`ObstacleTop${i}`].body, pipeSizePos.pipeTop.pos);
+      Matter.Body.setPosition(entities[`ObstacleBottom${i}`].body, pipeSizePos.pipeBottom.pos);
+    }
+    Matter.Body.translate(entities[`ObstacleTop${i}`].body, { x: -3, y: 0 });
+    Matter.Body.translate(entities[`ObstacleBottom${i}`].body, { x: -3, y: 0 });
+  }
 
   return entities;
 };
