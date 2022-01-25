d1e086af162506fc0b36086a44be89135924c73b
akmalsyrf <dev.akmal69@gmail.com>
2022-01-25 12:53:34 +0700

    finish functionalities

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { GameEngine } from "react-native-game-engine";
 
 import entities from "./entities";
@@ -11,11 +11,11 @@ export default function App() {
   const [gameEngine, setGameEngine] = useState(null);
   const [currentPoints, setCurrentPoints] = useState(0);
   useEffect(() => {
-    setRunning(true);
+    setRunning(false);
   }, []);
   return (
     <View style={{ flex: 1 }}>
-      <StatusBar style="auto" hidden />
+      <Text style={{ textAlign: "center", fontSize: 40, fontWeight: "bold", margin: 20 }}>{currentPoints}</Text>
       <GameEngine
         ref={(ref) => setGameEngine(ref)}
         systems={[Physics]}
@@ -26,7 +26,6 @@ export default function App() {
             case "game_over":
               setRunning(false);
               gameEngine.stop();
-              setCurrentPoints(0);
               break;
             case "new_point":
               setCurrentPoints(currentPoints + 1);
@@ -34,7 +33,23 @@ export default function App() {
           }
         }}
         style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }}
-      ></GameEngine>
+      >
+        <StatusBar style="auto" hidden />
+      </GameEngine>
+      {!running ? (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <TouchableOpacity
+            onPress={() => {
+              setCurrentPoints(0);
+              setRunning(true);
+              gameEngine.swap(entities());
+            }}
+            style={{ backgroundColor: "black", paddingHorizontal: 30, paddingVertical: 10 }}
+          >
+            <Text style={{ color: "white", fontWeight: "bold", fontSize: 30 }}>START GAME</Text>
+          </TouchableOpacity>
+        </View>
+      ) : null}
     </View>
   );
 }
